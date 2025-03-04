abbac8c5688ddc0e0abea37368f457b30d1286af
pepega <aristockij@gmail.com>
2025-03-04 14:23:33 +0300

    add reset form after submit

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import MaskedInput from "react-text-mask";
 
 const index = () => {
+  const [formSendSuccess, setFormSendSuccess] = useState(false);
+
   const phoneRegExp =
     /\+7-\(?([0-9]{3})\)?-([ .-]?)([0-9]{3})-([0-9]{2})-([0-9]{2})/;
 
@@ -61,8 +64,12 @@ const index = () => {
     const result = await response.json();
 
     if (result.ok) {
-      console.log("форма успешно отправилась!");
+      setFormSendSuccess(true);
+      setTimeout(() => {
+        setFormSendSuccess(false);
+      }, 5000);
     } else {
+      setFormSendSuccess(false);
       console.log("форма не отправилась!");
     }
   };
@@ -71,9 +78,9 @@ const index = () => {
     <Formik
       initialValues={{ name: "", question: "", phone: "" }}
       validationSchema={validation}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log(values);
+      onSubmit={(values, { setSubmitting, resetForm }) => {
         sendFormVal(values);
+        resetForm();
       }}
     >
       {({ errors, isSubmitting }) => (
@@ -82,38 +89,47 @@ const index = () => {
             Оставьте свою заявку <br />и мастер перезвонит вам в течении 10
             минут
           </h2>
-          <div className='form__wrap'>
-            <div className='form'>
-              <div className='input'>
-                <label htmlFor='name'>Введите имя:</label>
-                <Field id='name' name='name' />
-                {errors && <div className='error'>{errors.name}</div>}
+          {formSendSuccess ? (
+            <div className='form__success'>
+              <div className='form__success--img'>
+                <img src='/icons/checked.svg' alt='checked' />
               </div>
-
-              <div className='input'>
-                <label htmlFor='question'>Опишите проблему:</label>
-                <Field id='question' name='question' />
-              </div>
-
-              <div className='input'>
-                <label htmlFor='phone'>Ваш телефон:</label>
-                <Field name='phone'>
-                  {({ field, value }) => (
-                    <MaskedInput
-                      mask={phoneMask}
-                      {...field}
-                      className='textfield'
-                    />
-                  )}
-                </Field>
-                {errors && <div className='error'>{errors.phone}</div>}
-              </div>
-
-              <button className='btn ' type='submit' disabled={isSubmitting}>
-                Оставить заявку
-              </button>
+              Ваша заявка принята!
             </div>
-          </div>
+          ) : (
+            <div className='form__wrap'>
+              <div className='form'>
+                <div className='input'>
+                  <label htmlFor='name'>Введите имя:</label>
+                  <Field id='name' name='name' />
+                  {errors && <div className='error'>{errors.name}</div>}
+                </div>
+
+                <div className='input'>
+                  <label htmlFor='question'>Опишите проблему:</label>
+                  <Field id='question' name='question' />
+                </div>
+
+                <div className='input'>
+                  <label htmlFor='phone'>Ваш телефон:</label>
+                  <Field name='phone'>
+                    {({ field, value }) => (
+                      <MaskedInput
+                        mask={phoneMask}
+                        {...field}
+                        className='textfield'
+                      />
+                    )}
+                  </Field>
+                  {errors && <div className='error'>{errors.phone}</div>}
+                </div>
+
+                <button className='btn ' type='submit' disabled={isSubmitting}>
+                  Оставить заявку
+                </button>
+              </div>
+            </div>
+          )}
         </Form>
       )}
     </Formik>
