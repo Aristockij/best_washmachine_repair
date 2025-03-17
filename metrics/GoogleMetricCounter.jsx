9223479a9e0fa4686abeb74a81ce084c5b16b335
pepega <aristockij@gmail.com>
2025-03-17 12:57:18 +0300

    replace metric to client component

--- metrics/GoogleMetricCounter.jsx
+++ metrics/GoogleMetricCounter.jsx
@@ -1,23 +1,24 @@
-import Script from "next/script";
+"use client";
+import { useEffect } from "react";
 
 function GoogleMetricCounter() {
-  return (
-    <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GM_ID}`}
-        strategy='afterInteractive'
-      />
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GM_ID}`;
+    script1.async = true;
+    document.head.appendChild(script1);
 
-            gtag('config', '${process.env.NEXT_PUBLIC_GM_ID}');
-        `}
-      </Script>
-    </>
-  );
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GM_ID}');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
+  return null;
 }
 
 export default GoogleMetricCounter;
--- metrics/YandexMetricCounter.jsx
+++ metrics/YandexMetricCounter.jsx
@@ -1,22 +1,38 @@
-import Script from "next/script";
+"use client";
+import { useEffect } from "react";
 
 function YandexMetricCounter() {
-  return (
-    <Script id='ya-metrika-counter' strategy='afterInteractive'>
-      {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      m[i].l=1*new Date();
-      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  useEffect(() => {
+    if (!process.env.NEXT_PUBLIC_YA_ID) return;
 
-      ym(${process.env.NEXT_PUBLIC_YA_ID}, "init", {
-            defer: true,
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true
-      })`}
-    </Script>
-  );
+    (function (m, e, t, r, i, k, a) {
+      m[i] =
+        m[i] ||
+        function () {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) {
+          return;
+        }
+      }
+      (k = e.createElement(t)),
+        (a = e.getElementsByTagName(t)[0]),
+        (k.async = 1),
+        (k.src = r),
+        a.parentNode.insertBefore(k, a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    window.ym(`${process.env.NEXT_PUBLIC_YA_ID}`, "init", {
+      defer: true,
+      clickmap: true,
+      trackLinks: true,
+      accurateTrackBounce: true,
+    });
+  }, []);
+
+  return null;
 }
 
 export default YandexMetricCounter;
